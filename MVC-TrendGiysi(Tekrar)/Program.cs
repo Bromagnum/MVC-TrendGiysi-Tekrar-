d4958f985a1cdb91778ed8dd0bab94d4bd66d8af
feyza <feyza@FEYZALAPTOP>
2025-07-17 18:56:48 +0300

    feyzaconnection eklendi

--- MVC-TrendGiysi(Tekrar)/Program.cs
+++ MVC-TrendGiysi(Tekrar)/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IProductService, ProductService>();
 
 //AddConnection
-string connectionString = builder.Configuration.GetConnectionString("AtakanConnection");
+string connectionString = builder.Configuration.GetConnectionString("FeyzaConnection");
 //AddDbcontext
 builder.Services.AddDbContext<GiysiContext>(options => options.UseSqlServer(connectionString));
 
